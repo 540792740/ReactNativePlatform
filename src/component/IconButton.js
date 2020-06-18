0a909598210fcf845387cdd4e546f7648855f211
Super. W <31707135+540792740@users.noreply.github.com>
2020-06-18 09:42:39 -0400

    Add files via upload

---
+++ src/component/IconButton.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import {View, Text, StyleSheet, TextInput, TouchableOpacity} from 'react-native';
+import Icon from 'react-native-ionicons';
+
+
+export function IconButton({name, style, onPress}) {
+    return  (
+        <TouchableOpacity style={[styles.container, style]} onPress={onPress}>
+            <Icon name={'close-circle-outline'}/>
+        </TouchableOpacity>
+    )}
+
+const styles = StyleSheet.create({
+    container:{
+
+    }
+})
+
